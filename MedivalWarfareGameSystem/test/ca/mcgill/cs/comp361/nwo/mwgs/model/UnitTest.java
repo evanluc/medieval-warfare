9540711483bcac1eed2a490333a29ea58b08bb49
David Zhou <davidzhou94@gmail.com>
2015-02-20 21:42:22 -0500

    Added two test classes, one of them is empty

---
+++ MedivalWarfareGameSystem/test/ca/mcgill/cs/comp361/nwo/mwgs/model/UnitTest.java
@@ -0,0 +1,16 @@
+package ca.mcgill.cs.comp361.nwo.mwgs.model;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class UnitTest
+{
+
+    @Test
+    public void test()
+    {
+        fail("Not yet implemented");
+    }
+
+}
