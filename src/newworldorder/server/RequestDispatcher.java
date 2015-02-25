fa26fbccdc17cf934a59e81ecd5ca833c90fd38c
Andrew Martin <andrewsmartin.mg@gmail.com>
2015-02-24 20:06:34 -0500

    RequestDispatcher is now a CommandExecutor instead of a direct MessageHandler. Main application now just a CommandHandler, with the request dispatcher injected.

--- src/newworldorder/server/RequestDispatcher.java
+++ src/newworldorder/server/RequestDispatcher.java
@@ -1,38 +1,26 @@
 package newworldorder.server;
 
-import java.io.ByteArrayInputStream;
-import java.io.IOException;
-import java.io.ObjectInputStream;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
-import newworldorder.common.network.MessageHandler;
+import newworldorder.common.network.message.CommandExecutor;
 import newworldorder.common.network.message.RemoteCommand;
 import newworldorder.common.service.ServiceLocator;
 
-public class RequestDispatcher implements MessageHandler {
+public class RequestDispatcher implements CommandExecutor {
 
 	private ExecutorService threadPool = Executors.newCachedThreadPool();
 	private ServiceLocator locator = ServiceLocator.getInstance();
 
 	@Override
-	public void handle(byte[] message) {
-		try {
-			ObjectInputStream ostream = new ObjectInputStream(new ByteArrayInputStream(message));
-			RemoteCommand command = (RemoteCommand) ostream.readObject();
-			command.setServiceLocator(locator);
-
-			threadPool.execute(new Runnable() {
-				@Override
-				public void run() {
-					command.execute();
-				}
-			});
-		} catch (IOException e) {
-			e.printStackTrace();
-		} catch (ClassNotFoundException e) {
-			e.printStackTrace();
-		}
+	public void execute(RemoteCommand command) {
+		command.setServiceLocator(locator);
+		
+		threadPool.execute(new Runnable() {
+			@Override
+			public void run() {
+				command.execute();
+			}
+		});
 	}
-
 }
--- src/newworldorder/server/ServerMain.java
+++ src/newworldorder/server/ServerMain.java
@@ -1,14 +1,17 @@
 package newworldorder.server;
 
-import newworldorder.common.network.ActorFactory;
 import newworldorder.common.network.MessageConsumer;
 import newworldorder.common.network.MessageHandler;
+import newworldorder.common.network.factory.ActorFactory;
+import newworldorder.common.network.message.CommandExecutor;
+import newworldorder.common.network.message.CommandHandler;
 
 public class ServerMain {
 
 	public static void main(String[] args) throws Exception {
-		MessageHandler dispatcher = new RequestDispatcher();
-		MessageConsumer consumer = ActorFactory.createDirectConsumer("localhost", "requestQueue", dispatcher);
+		CommandExecutor executor = new RequestDispatcher();
+		MessageHandler requestHandler = new CommandHandler(executor);
+		MessageConsumer consumer = ActorFactory.createDirectConsumer("localhost", "requestQueue", requestHandler);
 		consumer.consumeMessages();
 	}
 
