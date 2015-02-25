e7a0ff2de9eef86998d2ff2d159042e918446c62
Andrew Martin <andrewsmartin.mg@gmail.com>
2015-02-24 19:08:30 -0500

    Changed host to server ip and removed unused imports. TODO: make producer code more wieldy for the client, perhaps abstract host and serialization away.

--- MedivalWarfareGameSystem/src/newworldorder/ui/LoginPanel.java
+++ MedivalWarfareGameSystem/src/newworldorder/ui/LoginPanel.java
@@ -4,13 +4,11 @@ import java.awt.BorderLayout;
 import java.awt.Color;
 import java.awt.Component;
 import java.awt.Font;
-import java.awt.GridLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.io.ObjectOutputStream;
-import java.nio.charset.StandardCharsets;
 
 import javax.swing.BoxLayout;
 import javax.swing.JButton;
@@ -22,67 +20,63 @@ import newworldorder.common.network.ActorFactory;
 import newworldorder.common.network.MessageProducer;
 import newworldorder.common.network.message.PrintCommand;
 
-public class LoginPanel extends JPanel{
+public class LoginPanel extends JPanel {
 	/**
 	 * 
 	 */
 	private static final long serialVersionUID = 1L;
 	private MainView aMainView;
-	public LoginPanel(MainView pMainView){
+
+	public LoginPanel(MainView pMainView) {
 		super();
 		aMainView = pMainView;
-		setLayout( new BorderLayout() );
-	
+		setLayout(new BorderLayout());
+
 		aMainView.setName(null);
 		JPanel northPanel = new JPanel();
 		JLabel jlabel = new JLabel("Please Login");
-	    jlabel.setFont(new Font("Calibri",1,60));
-	    northPanel.add(jlabel);
-	    northPanel.setOpaque(false);
-	    add(northPanel, BorderLayout.NORTH);
-		
+		jlabel.setFont(new Font("Calibri", 1, 60));
+		northPanel.add(jlabel);
+		northPanel.setOpaque(false);
+		add(northPanel, BorderLayout.NORTH);
+
 		JButton back = new JButton("Back");
 		back.setBackground(new Color(220, 20, 60));
-	    back.setForeground(Color.WHITE);
-	    back.setFocusPainted(false);
-	    back.setFont(new Font("Calibri", Font.BOLD, 20));
+		back.setForeground(Color.WHITE);
+		back.setFocusPainted(false);
+		back.setFont(new Font("Calibri", Font.BOLD, 20));
 		back.setAlignmentX(Component.CENTER_ALIGNMENT);
-		back.addActionListener(new ActionListener(){
+		back.addActionListener(new ActionListener() {
 			@Override
-			public void actionPerformed(ActionEvent e)
-			{
+			public void actionPerformed(ActionEvent e) {
 				aMainView.setMenuFromLogin();
-			}	
+			}
 		});
-		
+
 		JPanel subCenterPanel = new JPanel();
 		final JTextField field = new JTextField(20);
 		field.setText(aMainView.getName());
 		field.setAlignmentX(Component.CENTER_ALIGNMENT);
-		field.addActionListener( new ActionListener()
-		{
+		field.addActionListener(new ActionListener() {
 			@Override
-			public void actionPerformed(ActionEvent pEvent)
-			{
+			public void actionPerformed(ActionEvent pEvent) {
 				aMainView.setName(field.getText());
 			}
-		});	
+		});
 		JButton enter = new JButton("Enter");
-        enter.setBackground(new Color(59, 89, 182));
-        enter.setForeground(Color.WHITE);
-        enter.setFocusPainted(false);
-        enter.setFont(new Font("Calibri", Font.BOLD, 20));
-        enter.setAlignmentX(Component.CENTER_ALIGNMENT);
-		enter.addActionListener(new ActionListener()
-		{
+		enter.setBackground(new Color(59, 89, 182));
+		enter.setForeground(Color.WHITE);
+		enter.setFocusPainted(false);
+		enter.setFont(new Font("Calibri", Font.BOLD, 20));
+		enter.setAlignmentX(Component.CENTER_ALIGNMENT);
+		enter.addActionListener(new ActionListener() {
 			@Override
-			public void actionPerformed(ActionEvent pEvent)
-			{
+			public void actionPerformed(ActionEvent pEvent) {
 				aMainView.setName(field.getText());
 				aMainView.setJoinGame();
-				
+
 				try {
-					MessageProducer producer = ActorFactory.createDirectProducer("localhost", "requestQueue");
+					MessageProducer producer = ActorFactory.createDirectProducer("142.157.148.114", "requestQueue");
 					PrintCommand printCommand = new PrintCommand(aMainView.getName(), "hi");
 					ByteArrayOutputStream bstream = new ByteArrayOutputStream();
 					ObjectOutputStream ostream = new ObjectOutputStream(bstream);
@@ -100,18 +94,18 @@ public class LoginPanel extends JPanel{
 		subCenterPanel.add(field);
 		subCenterPanel.add(enter);
 		subCenterPanel.setOpaque(false);
-		
+
 		JPanel centerPanel = new JPanel();
 		centerPanel.setLayout(new BoxLayout(centerPanel, BoxLayout.Y_AXIS));
 		centerPanel.setOpaque(false);
 		centerPanel.add(subCenterPanel);
 		add(centerPanel, BorderLayout.CENTER);
 		JPanel southPanel = new JPanel();
-		southPanel.setLayout(new BoxLayout(southPanel, BoxLayout.Y_AXIS));	
-		
-        southPanel.add(back);
+		southPanel.setLayout(new BoxLayout(southPanel, BoxLayout.Y_AXIS));
+
+		southPanel.add(back);
 		southPanel.setOpaque(false);
 		add(southPanel, BorderLayout.SOUTH);
-	    setOpaque(false);
-	}    
+		setOpaque(false);
+	}
 }
