351f7ddfc02b2d108e253eda9ea016947983d15f
Andrew Martin <andrewsmartin.mg@gmail.com>
2015-03-02 09:36:22 -0500

    Another relatively large commit. Server now uses the spring-amqp api.

--- medieval-warfare-server/src/main/java/newworldorder/server/ServerConfig.java
+++ medieval-warfare-server/src/main/java/newworldorder/server/ServerConfig.java
@@ -1,19 +1,53 @@
 package newworldorder.server;
 
-import java.io.IOException;
-
-import newworldorder.common.network.IRoutingProducer;
-import newworldorder.common.network.factory.ActorFactory;
-
+import org.springframework.amqp.core.MessageListener;
+import org.springframework.amqp.rabbit.connection.CachingConnectionFactory;
+import org.springframework.amqp.rabbit.connection.ConnectionFactory;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
+
+import newworldorder.common.network.message.CommandExecutor;
+import newworldorder.common.network.message.CommandListener;
 
 @Configuration
-@ComponentScan
+@PropertySource("classpath:/application.properties")
 public class ServerConfig {
+
+	@Value("${rabbitmq.host}")
+	String host;
+
+	@Value("${rabbitmq.port}")
+	int port;
+
+	@Value("${rabbitmq.username}")
+	String username;
+
+	@Value("${rabbitmq.password}")
+	String password;
+
+	@Autowired
+	private RabbitTemplate rabbitTemplate;
+
 	@Bean
-	public IRoutingProducer gameNotifier() throws IOException {
-		return ActorFactory.createRoutingProducer("localhost", "notifyExchange");
+	ConnectionFactory connectionFactory() {
+		CachingConnectionFactory connectionFactory = new CachingConnectionFactory(host, port);
+		connectionFactory.setUsername("newworldorder");
+		connectionFactory.setPassword("warfare");
+		return connectionFactory;
+	}
+
+	@Bean
+	MessageListener commandListener(CommandExecutor executor) {
+		return new CommandListener(executor);
+	}
+
+	@Bean
+	public static PropertySourcesPlaceholderConfigurer placeHolderConfigurer() {
+		return new PropertySourcesPlaceholderConfigurer();
 	}
 }
