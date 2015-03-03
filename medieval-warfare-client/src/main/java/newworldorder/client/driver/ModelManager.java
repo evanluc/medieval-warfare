4bf7feaff4e5ed52cb04843da640d5631294b1f2
David Zhou <davidzhou94@gmail.com>
2015-03-03 11:06:14 -0500

    Began implementation of ModelManager and made a slight change to UIActionType enum to also include unit and village types, this way the GUI does not need to know about those enums for communication to the model.

--- medieval-warfare-client/src/main/java/newworldorder/client/driver/ModelManager.java
+++ medieval-warfare-client/src/main/java/newworldorder/client/driver/ModelManager.java
@@ -3,14 +3,21 @@ package newworldorder.client.driver;
 import java.util.List;
 
 import newworldorder.client.model.GameEngine;
+import newworldorder.client.model.Tile;
+import newworldorder.client.model.Unit;
+import newworldorder.client.model.UnitType;
+import newworldorder.client.model.Village;
+import newworldorder.client.model.VillageType;
 
 public class ModelManager implements IModelCommunicator {
 	
 	ModelManager instance;
 	GameEngine engine;
+	boolean gameRunning;
 	
 	private ModelManager() {
 		engine = new GameEngine();
+		gameRunning = false;
 	}
 	
 	@Override
@@ -21,10 +28,73 @@ public class ModelManager implements IModelCommunicator {
 		return instance;
 	}
 
+
+	@Override
+	public void informOfUserAction(UIActionType action) {
+		// TODO Auto-generated method stub
+		
+	}
+	
+	/**
+	 * This method handles BUILDROAD, BUILDTOWER, CULTIVATEMEADOW, UPGRADEUNIT*, and 
+	 * UPGRADEVILLAGE*. All other UIActionTypes are ignored. 
+	 * @param action The UIActionType
+	 * @param x x-coordinate
+	 * @param y y-coordinate
+	 */
 	@Override
 	public void informOfUserAction(UIActionType action, int x, int y) {
-		// TODO Auto-generated method stub
-
+		// TODO Complete implementation
+		
+		if (!gameRunning)
+			return;
+		
+		Unit u;
+		Village v;
+		Tile t = engine.getGameState().getMap().getTile(x, y);
+		
+		switch (action) {
+		case BUILDROAD:
+			u = t.getUnit();
+			if (u != null)
+				engine.buildRoad(u);
+			break;
+		case BUILDTOWER:
+			engine.buildTower(t);
+			break;
+		case CULTIVATEMEADOW:
+			u = t.getUnit();
+			if (u != null)
+				engine.cultivateMeadow(u);
+			break;
+		case UPGRADEUNITSOLDIER:
+			u = t.getUnit();
+			if (u != null && Unit.unitLevel(u.getUnitType()) < Unit.unitLevel(UnitType.SOLDIER))
+				engine.upgradeUnit(u, UnitType.SOLDIER);
+			break;
+		case UPGRADEUNITINFANTRY:
+			u = t.getUnit();
+			if (u != null && Unit.unitLevel(u.getUnitType()) < Unit.unitLevel(UnitType.INFANTRY))
+				engine.upgradeUnit(u, UnitType.INFANTRY);
+			break;
+		case UPGRADEUNITKNIGHT:
+			u = t.getUnit();
+			if (u != null && Unit.unitLevel(u.getUnitType()) < Unit.unitLevel(UnitType.KNIGHT))
+				engine.upgradeUnit(u, UnitType.KNIGHT);
+			break;
+		case UPGRADEVILLAGETOWN:
+			v = t.getVillage();
+			if (v != null && Village.villageLevel(v.getVillageType()) < Village.villageLevel(VillageType.TOWN))
+				engine.upgradeVillage(v, VillageType.TOWN);
+			break;
+		case UPGRADEVILLAGEFORT:
+			v = t.getVillage();
+			if (v != null && Village.villageLevel(v.getVillageType()) < Village.villageLevel(VillageType.FORT))
+				engine.upgradeVillage(v, VillageType.FORT);
+			break;
+		default:
+			break;
+		}
 	}
 
 	@Override
@@ -63,11 +133,6 @@ public class ModelManager implements IModelCommunicator {
 		
 	}
 
-	@Override
-	public void informOfUserAction(UIActionType action) {
-		// TODO Auto-generated method stub
-		
-	}
 
 	@Override
 	public void connect(String serverAddress) {
