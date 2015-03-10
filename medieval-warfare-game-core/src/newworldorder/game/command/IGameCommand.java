90d49edc827bc20ea5d0adf73ea02e5732c120ea
Andrew Martin <andrewsmartin.mg@gmail.com>
2015-03-10 19:04:21 -0400

    Made GameCommands serializable.

--- medieval-warfare-game-core/src/newworldorder/game/command/IGameCommand.java
+++ medieval-warfare-game-core/src/newworldorder/game/command/IGameCommand.java
@@ -1,8 +1,10 @@
 package newworldorder.game.command;
 
+import java.io.Serializable;
+
 import newworldorder.common.network.command.Command;
 import newworldorder.game.model.GameEngine;
 
-public interface IGameCommand extends Command {
+public interface IGameCommand extends Command, Serializable {
 	public void setGameEngine(GameEngine engine);
 }
